236f3da9c75a4d2567adc7ff13a3e41be255e93d
Tetiana <dak7214@gmail.com>
2025-10-24 21:23:34 +0300

    add files

---
+++ src/components/App/App.tsx
@@ -0,0 +1,78 @@
+import { useState, useEffect } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import Loader from '../Loader/Loader';
+import MovieGrid from '../MovieGrid/MovieGrid';
+import SearchBar from '../SearchBar/SearchBar';
+import ErrorMessage from '../ErrorMessage/ErrorMessage';
+import MovieModal from '../MovieModal/MovieModal';
+import ReactPaginate from 'react-paginate';
+import css from './App.module.css';
+import type { Movie } from '../../types/movie';
+import { fetchMovies } from '../../services/movieServices';
+import { Toaster, toast } from 'react-hot-toast';
+
+function App() {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
+  const [page, setPage] = useState(1);
+
+  const { data, isLoading, isError, isFetched } = useQuery({
+    queryKey: ['movies', searchQuery, page],
+    queryFn: () => fetchMovies(searchQuery, page),
+    enabled: !!searchQuery,
+    placeholderData: (previousData) => previousData,
+  });
+
+  const handleSearch = (query: string) => {
+    if (!query.trim()) {
+      toast.error('Please enter a valid search query.');
+      return;
+    }
+    setSearchQuery(query);
+    setPage(1);
+  };
+
+  const handleSelectMovie = (movie: Movie) => setSelectedMovie(movie);
+  const handleCloseModal = () => setSelectedMovie(null);
+
+  useEffect(() => {
+    if (isFetched && (data?.results?.length ?? 0) === 0) {
+      toast.error('No movies found for your request.');
+    }
+  }, [isFetched, data]);
+
+  return (
+    <>
+      <SearchBar onSubmit={handleSearch} />
+
+      {isLoading && <Loader />}
+      {isError && <ErrorMessage />}
+
+      {!isLoading && !isError && (data?.results?.length ?? 0) > 0 && (
+        <>
+          {(data?.total_pages ?? 0) > 1 && (
+            <ReactPaginate
+              pageCount={data?.total_pages ?? 0}
+              pageRangeDisplayed={5}
+              marginPagesDisplayed={1}
+              onPageChange={({ selected }) => setPage(selected + 1)}
+              forcePage={page - 1}
+              containerClassName={css.pagination}
+              activeClassName={css.active}
+              nextLabel="→"
+              previousLabel="←"
+            />
+          )}
+          <MovieGrid movies={data?.results ?? []} onSelect={handleSelectMovie} />
+        </>
+      )}
+
+      <Toaster position="top-right" />
+      {selectedMovie && (
+        <MovieModal movie={selectedMovie} onClose={handleCloseModal} />
+      )}
+    </>
+  );
+}
+
+export default App;
---
+++ src/services/movieServices.ts
@@ -0,0 +1,40 @@
+import axios from 'axios';
+import type { AxiosResponse } from 'axios';
+import type { Movie } from '../types/movie';
+
+const BASE_URL = 'https://api.themoviedb.org/3';
+const TOKEN = import.meta.env.VITE_TMDB_TOKEN;
+
+interface MovieResponse {
+  page: number;
+  results: Movie[];
+  total_pages: number;
+  total_results: number;
+}
+
+if (!TOKEN) {
+  throw new Error('TMDB token is missing! Please add VITE_TMDB_TOKEN to .env');
+}
+
+const api = axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    Authorization: `Bearer ${TOKEN}`,
+  },
+});
+
+export async function fetchMovies(
+  query: string,
+  page: number = 1
+): Promise<MovieResponse> {
+  const response: AxiosResponse<MovieResponse> = await api.get<MovieResponse>('/search/movie', {
+    params: {
+      query,
+      page,
+      include_adult: false,
+      language: 'en-US',
+    },
+  });
+
+  return response.data;
+}
